fb71828949ce721e08983de311c9f092d7916df3
e1iaz <ncabhd@hotmail.com>
2024-06-14 08:28:54 +0800

    add translation.

--- backend/app.py
+++ backend/app.py
@@ -2,20 +2,22 @@ from flask import Flask, request
 from flask_cors import CORS
 import json
 
+from translation.src.translation_agent.utils import translate
+
 app = Flask(__name__)
 CORS(app)
 
 
 @app.route("/api/translate", methods=['POST'])
-def translate():
+def translation():
     if request.method == "POST":
         from_text = request.json.get("from")
         to_text = request.json.get("to")
         source_text = request.json.get("source")
-        translation = "hello"
+        result = translate(from_text, to_text, source_text, "")
     else:
-        translation = "!!!ERROR!!!"
-    data = {"translation": translation}
+        result = "!!!ERROR!!!"
+    data = {"translation": result}
     return json.dumps(data)
 
 
